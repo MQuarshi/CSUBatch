c324927ff8006c54b718ae621401a756727bfda3
kendrickgholston <kengholston8@gmail.com>
2018-09-21 14:19:45 -0400

    Starting Thread creation & synchronization

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "Queue.h"
+#include <pthread.h>
+
+pthread_mutex_t queueMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condA = PTHREAD_COND_INITIALIZER;
+pthread_cond_t condB = PTHREAD_COND_INITIALIZER;
+pthread_t tid;
 
 int main() {
 
@@ -18,3 +24,34 @@ int main() {
     printf("Count: %d", head->count);
     return 0;
 }
+
+void createModules(void *module1, void *module2) {
+
+    pthread_create(&tid, NULL, module1, (void *) &tid);
+    pthread_create(&tid, NULL, module2, (void *) &tid);
+
+}
+
+//void *schedulerMod () {
+
+//int resVal;
+
+//Implementation not done yet
+
+//resVal = pthread_cond_signal(&condB);
+
+//resVal = pthread_mutex_lock(&queueMutex);
+
+//while (pthread_cond_wait(&condA, &queueMutex) != 0)
+
+// }
+
+//void *dispatcherMod () {
+
+//Implementation not done yet
+
+//while (pthread_cond_wait(&condB, &queueMutex) != 0)
+
+// rValue = pthread_mutex_unlock(&mutex);
+
+//}
